a721a45a42cda2cb89057e0c39479467345ef4b3
nico-tarabao <155532365+nico-tarabao@users.noreply.github.com>
2024-10-24 13:54:45 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -7,8 +7,8 @@
 // @match        https://kaiber.ai/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kaiber.ai
 // @grant        none
-// @updateURL 
-// @downloadURL
+// @updateURL    https://raw.githubusercontent.com/nico-tarabao/Kaiber/refs/heads/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/nico-tarabao/Kaiber/refs/heads/main/script.user.js
 // ==/UserScript==
 
 (function() {
