10e4ca58ffdd92ca25943ba95a5fc53109c99209
nico-tarabao <155532365+nico-tarabao@users.noreply.github.com>
2024-10-24 13:51:55 +0200

    Update and rename script to script.user.js

--- script.user.js
+++ script.user.js
@@ -7,6 +7,8 @@
 // @match        https://kaiber.ai/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kaiber.ai
 // @grant        none
+// @updateURL 
+// @downloadURL
 // ==/UserScript==
 
 (function() {
